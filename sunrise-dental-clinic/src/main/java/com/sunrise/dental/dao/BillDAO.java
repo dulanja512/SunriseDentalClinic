077ba66b5e5c34636f0f56c610e6a6b230dfd0c0
dulanja512 <edulanjan28@gmail.com>
2026-09-04 14:41:19 +0530

    Update BillDAO.java

--- sunrise-dental-clinic/src/main/java/com/sunrise/dental/dao/BillDAO.java
+++ sunrise-dental-clinic/src/main/java/com/sunrise/dental/dao/BillDAO.java
@@ -1,6 +1,9 @@
 package com.sunrise.dental.dao;
+
 import com.sunrise.dental.model.Bill;
+
 public interface BillDAO {
     int save(Bill b);
+    boolean update(Bill b);
     Bill findByAppointment(int id);
 }
