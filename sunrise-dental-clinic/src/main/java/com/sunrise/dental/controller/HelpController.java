9eb8fa210711caed8450d1ec5d5b32673c7c8e0f
dulanja512 <edulanjan28@gmail.com>
2026-09-04 14:39:44 +0530

    Update HelpController.java

--- sunrise-dental-clinic/src/main/java/com/sunrise/dental/controller/HelpController.java
+++ sunrise-dental-clinic/src/main/java/com/sunrise/dental/controller/HelpController.java
@@ -3,6 +3,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 @WebServlet("/help") public class HelpController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
     protected void doGet(HttpServletRequest r,HttpServletResponse s)throws ServletException,java.io.IOException {
         r.getRequestDispatcher("/WEB-INF/view/help.jsp").forward(r,s);
     }
