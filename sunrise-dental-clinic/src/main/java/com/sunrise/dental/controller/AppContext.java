59660cbfb8b617b6235d8a0e9bc042d23f31f408
dulanja512 <edulanjan28@gmail.com>
2026-09-04 14:34:32 +0530

    Update AppContext.java

--- sunrise-dental-clinic/src/main/java/com/sunrise/dental/controller/AppContext.java
+++ sunrise-dental-clinic/src/main/java/com/sunrise/dental/controller/AppContext.java
@@ -2,7 +2,8 @@ package com.sunrise.dental.controller;
 import com.sunrise.dental.dao.*;
 import com.sunrise.dental.service.*;
 public final class AppContext {
-    public static final AuthService AUTH=new AuthService(new UserDAOImpl());
+    public static final UserDAO USER_DAO=new UserDAOImpl();
+    public static final AuthService AUTH=new AuthService(USER_DAO);
     public static final AppointmentDAO APPT_DAO=new AppointmentDAOImpl();
     public static final AppointmentService APPT=new AppointmentService(APPT_DAO);
     public static final DentistDAO DENTIST_DAO=new DentistDAOImpl();
