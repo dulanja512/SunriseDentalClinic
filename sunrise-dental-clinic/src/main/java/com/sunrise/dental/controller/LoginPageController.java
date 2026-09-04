2e13a042b627627d293f8e52f03766491f7b2b44
dulanja512 <edulanjan28@gmail.com>
2026-09-04 14:39:49 +0530

    Update LoginPageController.java

--- sunrise-dental-clinic/src/main/java/com/sunrise/dental/controller/LoginPageController.java
+++ sunrise-dental-clinic/src/main/java/com/sunrise/dental/controller/LoginPageController.java
@@ -3,6 +3,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 @WebServlet("/login") public class LoginPageController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
     protected void doGet(HttpServletRequest r,HttpServletResponse s)throws ServletException,java.io.IOException {
         r.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(r,s);
     }
